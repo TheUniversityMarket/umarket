311f4c374ca09c545eb468da9f144ea1ecf58629
JevonTwitty <twittyjevon@gmail.com>
2024-04-18 20:01:43 -0400

    Fixed Scaling

--- src/screens/UserLogin.tsx
+++ src/screens/UserLogin.tsx
@@ -54,7 +54,7 @@ function UserLogin( {navigation} ) {
 
             <View style={{flex: 1, backgroundColor: "transparent"}}></View> 
 
-            <View style={{flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "white", minWidth: 350, minHeight: 600, borderRadius: 20, borderWidth: 3, borderColor: "rgb(34 197 94)"}}>
+            <View style={{flex: 7, alignItems: "center", justifyContent: "center", backgroundColor: "white", minWidth: 350, maxWidth: 600, minHeight: 575, borderRadius: 20, borderWidth: 3, borderColor: "rgb(34 197 94)"}}>
               <View style={{width: "90%", flex: 1, justifyContent: "center"}}>
 
                   <View style={{alignItems: "center", justifyContent: "center"}}>
